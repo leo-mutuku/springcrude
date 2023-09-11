6e44903f059c749b30b04279506a520041cea28f
leo-mutuku <le.mutuku@gmail.com>
2023-09-11 07:54:04 +0300

    Update crude edpoints

--- src/main/java/com/crude/controller/EmployeeController.java
+++ src/main/java/com/crude/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.crude.exception.ResourceNotFoundException;
 import com.crude.model.Employee;
 import com.crude.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +30,29 @@ public class EmployeeController {
        Employee employee = employeeRepository.findById(id)
                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id " + id));
                return ResponseEntity.ok(employee);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+       Employee updateEmployee = employeeRepository.findById(id)
+               .orElseThrow(() -> new ResourceNotFoundException("Employee not exist witj the id" + id));
+       updateEmployee.setFirstName(employeeDetails.getFirstName());
+       updateEmployee.setLastname(employeeDetails.getLastname());
+       updateEmployee.setEmail(employeeDetails.getEmail());
+
+       employeeRepository.save(updateEmployee);
+
+       return ResponseEntity.ok(updateEmployee);
+
+    }
+    // Build delete employee
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteEmployeeBy(@PathVariable Long id) throws ResourceNotFoundException {
+       Employee employee = employeeRepository.findById(id)
+               .orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id" +id));
+       employeeRepository.delete(employee);
+
+       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 }
 
